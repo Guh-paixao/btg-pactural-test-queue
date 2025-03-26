2e6ac47b5ac88dccb7b56280982e200026835cef
Guh-paixao <gustavopaixao.dev@gmail.com>
2025-03-26 15:45:43 -0300

    feat: first commit

---
+++ src/main/java/br/com/gustavo/orderms/BtgPactualDesafioApplication.java
@@ -0,0 +1,13 @@
+package br.com.gustavo.orderms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BtgPactualDesafioApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BtgPactualDesafioApplication.class, args);
+    }
+
+}
---
+++ src/test/java/br/com/gustavo/orderms/BtgPactualDesafioApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.gustavo.orderms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BtgPactualDesafioApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
